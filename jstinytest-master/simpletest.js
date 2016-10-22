02b0ac8132fdfd7dd18cf9384ecf4a157b194a87
kennyklee <kennyklee@gmail.com>
2016-10-22 14:53:50 -0700

    Updated to latest-greatest SimpleTest.js

--- jstinytest-master/simpletest.js
+++ jstinytest-master/simpletest.js
@@ -1,60 +1,43 @@
-/**
- * Very simple in-browser unit-test library, with zero deps.
- *
- * Background turns green if all tests pass, otherwise red.
- * View the JavaScript console to see failure reasons.
- *
- * Example:
- *
- *   adder.js (code under test)
- *
- *     function add(a, b) {
- *       return a + b;
- *     }
- *
- *   adder-test.html (tests - just open a browser to see results)
- *
- *     <script src="tinytest.js"></script>
- *     <script src="adder.js"></script>
- *     <script>
- *
- *     tests({
- *
- *       'adds numbers': function() {
- *         eq(6, add(2, 4));
- *         eq(6.6, add(2.6, 4));
- *       },
- *
- *       'subtracts numbers': function() {
- *         eq(-2, add(2, -4));
- *       },
- *
- *     });
- *     </script>
- *
- * That's it. Stop using over complicated frameworks that get in your way.
- *
- * -Joe Walnes
- * MIT License. See https://github.com/joewalnes/jstinytest/
- */
-var TinyTest = {
+// DONE: Get successes to be green
+// DONE: One error per failure to the console
+// DONE: Make failures red
+// DONE: Show stack traces for failures
+// DONE: Only show stack traces if you click expand
+// DONE: Output summary statistics to the DOM
+// DONE: Refactor TinyTest
 
+var TinyTestHelper = {
+    renderStats: function(tests, failures) {
+        var numberOfTests = Object.keys(tests).length;
+        var successes = numberOfTests - failures;
+        var summaryText = "Ran " + numberOfTests + " tests: "
+                          + successes + " successes, "
+                          + failures + " failures"
+        var summaryElement = document.createElement('h1');
+        summaryElement.textContent = summaryText;
+        document.body.appendChild(summaryElement);
+    }
+}
+
+var TinyTest = {
     run: function(tests) {
         var failures = 0;
         for (var testName in tests) {
             var testAction = tests[testName];
             try {
                 testAction.apply(this);
-                console.log('Test:', testName, 'OK');
+                console.log('%c' + testName, "color: green;");
             } catch (e) {
                 failures++;
-                console.error('Test:', testName, 'FAILED', e);
+                console.groupCollapsed('%c' + testName, "color: red;");
                 console.error(e.stack);
+                console.groupEnd();
             }
         }
         setTimeout(function() { // Give document a chance to complete
             if (window.document && document.body) {
                 document.body.style.backgroundColor = (failures == 0 ? '#99ff99' : '#ff9999');
+                TinyTestHelper.renderStats(tests, failures);
             }
         }, 0);
     },
@@ -80,7 +63,6 @@ var TinyTest = {
             throw new Error('assertStrictEquals() "' + expected + '" !== "' + actual + '"');
         }
     },
-
 };
 
 var fail               = TinyTest.fail.bind(TinyTest),
@@ -88,4 +70,5 @@ var fail               = TinyTest.fail.bind(TinyTest),
     assertEquals       = TinyTest.assertEquals.bind(TinyTest),
     eq                 = TinyTest.assertEquals.bind(TinyTest), // alias for assertEquals
     assertStrictEquals = TinyTest.assertStrictEquals.bind(TinyTest),
+    eqs = TinyTest.assertStrictEquals.bind(TinyTest),
     tests              = TinyTest.run.bind(TinyTest);
