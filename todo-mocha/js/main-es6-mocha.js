fe8ac337bd59c30cbe5c8047d045983a845f778b
kennyklee <kennyklee@gmail.com>
2016-11-09 07:44:42 -0800

    wip

--- todo-mocha/js/main-es6-mocha.js
+++ todo-mocha/js/main-es6-mocha.js
@@ -1,5 +1,18 @@
-(function(){
- var plus = window.plus = function (one, two) {
-   return one + two;
- };
-})();
+//#!javascript
+// cow.js
+(function(exports) {
+  "use strict";
+
+  function Cow(name) {
+    this.name = name || "Anon cow";
+  }
+  exports.Cow = Cow;
+
+  Cow.prototype = {
+    greets: function(target) {
+      if (!target)
+        throw new Error("missing target");
+      return this.name + " greets " + target;
+    }
+  };
+})(this);
