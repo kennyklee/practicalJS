2976388dc510282c779f8ce97113d8b6c76d66c4
kennyklee <kennyklee@gmail.com>
2017-01-14 15:14:20 -0800

    First attempt at solution with tests.

--- isPrototypeOf/js/main.js
+++ isPrototypeOf/js/main.js
@@ -1,8 +1,24 @@
 (function (){
+    function isPrototypeOf(prototypeObj, object) {
+        //debugger;
+        // Not passing in an argument
+        if ((prototypeObj === undefined) || (prototypeObj === null)) {
+            return new TypeError("Error");
+        }
 
-    function isPrototypeOf() {
+        // if (prototypeObj.prototype === undefined) {
+        //     return "You did not pass in a prototype."
+        // }
 
+        if (prototypeObj === object) {
+            return false;
+        }
+
+        if (prototypeObj === Object.getPrototypeOf(object)) {
+            return true;  // If the prototype is the same, then they are NOT a prototype of each other.
+        }
+        return false;
     }
 
     window.isPrototypeOf = isPrototypeOf;
-})();
+}() );
--- isPrototypeOf/test/test.js
+++ isPrototypeOf/test/test.js
@@ -1,8 +1,47 @@
+tests({
+    'It should return an error when a prototype is not included in the argument.': function() {
+      var object1 = {};
+      var object2 = {};
 
+      eq(isPrototypeOf(null, object2), "TypeError: Error");
+    },
 
-// tests({
-//     'It should...': function() {
-//         eqs();
-//         assert();
-//     }
-// })
+    'It should return an error when prototype is \'undefined\'.': function() {
+      var object1 = {};
+      var object2 = {};
+
+      eq(isPrototypeOf(undefined, object2), "TypeError: Error");
+    },
+
+    'It should return true when object is in another object\'s prototype chain.': function() {
+      function Person(first, last, age) {
+        this.first = first;
+        this.last = last;
+        this.age = age;
+      }
+
+      var Kenny = new Person("kenny", "lee", 21);
+
+      eqs(isPrototypeOf(Person.prototype, Kenny), true);
+    },
+
+    'It should return false when object is NOT in another object\'s prototype chain.': function() {
+      function Person(first, last, age) {
+        this.first = first;
+        this.last = last;
+        this.age = age;
+      }
+
+      var Gordon = { first: "gordon", last: "zhu", age: 21 };
+
+      eqs(isPrototypeOf(Person.prototype, Gordon), false);
+    },
+
+    'It should return true since \'Object\' is a prototype of \'Array\'': function() {
+      eqs(isPrototypeOf(Object.prototype, Array.prototype), true);
+    },
+
+    'It should return false since \'Array\' is a prototype of \'Object\'': function() {
+      eqs(isPrototypeOf(Array.prototype, Object.prototype), false);
+    },
+})
